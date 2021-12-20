fd29c4b9799152f2bbbc43c946202ae75a52e7e6
allenbra17 <allenbra17@gmail.com>
2021-12-20 10:34:46 -0500

    trying again

--- APIChallenge/scripts.js
+++ APIChallenge/scripts.js
@@ -4,11 +4,14 @@ const APIKey = "k_d0n70ap7";
 let resultsElement = document.getElementById('results');
 let buttonElement = document.getElementById('pickMovies');
 let formElement = document.getElementById('movie-search');
+const startDate = document.querySelector('.startDate');
+const endDate = document.querySelector('.endDate');
+const genre = document.querySelector('.genre');
 
 
 // function movieData(movie, poster, rating) {
-//     //Clear previous results
-//     // resultsElement.innerHTML = '';
+//     Clear previous results
+//     resultsElement.innerHTML = '';
 //     let movieTitle = document.createElement('p');
 //     let moviePoster = document.createElement('p');
 //     let imDbRating = document.createElement('p');
@@ -22,26 +25,29 @@ let formElement = document.getElementById('movie-search');
 //     resultsElement.appendChild(imDbRating);
 // }
 
-// function pickMovies(startDate, endDate, genre) {
-//     let url =`${ baseURL }/${ APIKey }?release_date${ startDate },${ endDate }&genres=${ genre }`;
-//     console.log(url);
-//     fetch(url)
-//     .then(response => response.json())
-//     .then(jsonData => {
-//         console.log(jsonData);
-//         let movie = jsonData.results.title;
-//         let poster = jsonData.results.image;
-//         let rating = jsonData.results.imDbrating;
+function fetchResults(e) {
+    e.preventDefault();
+    let url =`${ baseURL.value }/${ APIKey }?release_date${ startDate.value },${ endDate }&genres=${ genre.value }`;
+    console.log(url);
+    fetch(url)
+    .then(response => response.json())
+    .then(jsonData => {
+        console.log(jsonData);
+        // let movie = jsonData.results.title;
+        // let poster = jsonData.results.image;
+        // let rating = jsonData.results.imDbrating;
 
-//         movieData(movie, poster, rating);
-//     })
-// }
+        // movieData(movie, poster, rating);
+    })
+}
 // function fetchMovie() {
 //     let startDate = new FormData(start);
 //     let endDate = new FormData(end);
 //     let genre = new FormData(genre);
 // }
 
-buttonElement.addEventListener('submit', () => {
-    console.log(startDate,endDate, genre)
-})
+// buttonElement.addEventListener('submit', () => {
+//     console.log(startDate,endDate, genre)
+    
+//     pickMovies(startDate, endDate, genre)
+// })
